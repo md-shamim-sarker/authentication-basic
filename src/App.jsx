aa314ea2ed7fed0113c66e03ed611cc16cbf5ecd
shamimspro <shamimspro@gmail.com>
2022-10-17 13:17:00 +0600

    registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import MainRouter from './routes/MainRouter';
+import React, {useState} from 'react';
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import app from './firebase/Firebase.config';
+
+const auth = getAuth(app);
 
 const App = () => {
+  const [success, setSuccess] = useState("");
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setSuccess("User Created Successfully.");
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setSuccess(error.message);
+      });
+    form.reset();
+  };
   return (
-    <div>
-      <MainRouter></MainRouter>
-    </div>
+    <form onSubmit={onSubmitHandler}>
+      <input type="email" name="email" id="emailId" placeholder='Email' /><br /><br />
+      <input type="password" name="password" id="passwordId" placeholder='Password' /> <br /><br />
+      <button>Register</button>
+      {
+        success && <div>{success}</div>
+      }
+    </form>
   );
 };
 
---
+++ src/firebase/Firebase.config.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBEzF8AvdrHmtVhCg5ESlnya_iArXNBtZE",
+    authDomain: "basic-auth-shamimspro.firebaseapp.com",
+    projectId: "basic-auth-shamimspro",
+    storageBucket: "basic-auth-shamimspro.appspot.com",
+    messagingSenderId: "541750192403",
+    appId: "1:541750192403:web:c4a11ec0989b87c15c52e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
